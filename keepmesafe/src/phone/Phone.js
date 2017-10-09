8ca2fa3ec0191454ca6a160aa911a12f506a6776
Victoria Espinosa <gvazquez@grupobmv.com.mx>
2017-10-09 16:36:31 -0500

    Phone

---
+++ keepmesafe/src/phone/Phone.js
@@ -0,0 +1,11 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These are your accountSid and authToken from https://www.twilio.com/console
+const accountSid = 'AC189fad9eef9a38b7c137caa7a4b9273b';
+const authToken = 'a5cc77d84f5b391b152f79c24a07a082';
+
+const client = require('twilio')(accountSid, authToken);
+
+client.lookups.v1
+  .phoneNumbers('+525510073148')
+  .fetch()
+  .then((number) => console.log(number.carrier.type, number.carrier.name));
